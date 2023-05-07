3c38c0acf0e4697419146a92f0a64eda0585c930
JesusDCalvo <jesusjj90@gmail.com>
2023-05-07 02:18:21 -0500

    avance
    Se montan la reseña, mision y vision.
    Se montan los modelos para cargar los productos por categoria y producto y se realiza la migración de estos.
    en admin de la app producto se registran las clases para el super usuario, pero presento conflicto aun no se soluciona

--- producto/admin.py
+++ producto/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-
-from .models import Producto
-admin.site.register(Producto)
+from .models import Category, Producto
 
 # Register your models here.
+class categoryAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', 'updated')
+
+class ProductoAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', 'updated')
+
+admin.site.register(Category, categoryAdmin)
+admin.site.register(Producto, ProductoAdmin)
--- producto/models.py
+++ producto/models.py
@@ -1,11 +1,29 @@
 from django.db import models
 
+#created your models here.
+class Category(models.Model):
+    name_cat = models.CharField(max_length=100, verbose_name="Nombre")
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de Creación")
+    updated = models.DateTimeField(auto_now=True, verbose_name="Fecha de Edición")
+    
+    class Meta:
+        verbose_name = "Categoria"
+        verbose_name_plural = "Categorias"
+        ordering = ['name_cat']
+
+        def __str__(self):
+            return self.name_cat
 class Producto(models.Model):
     reference=models.CharField(max_length=10)
     name=models.CharField(max_length=100)
-    description=models.CharField(max_length=200)
+    image=models.ImageField(verbose_name="Imagen", upload_to="productos", null=True)
     price=models.DecimalField(max_digits=6, decimal_places=2)
-    category=models.CharField(max_length=50)   
-    def __str__(self):
-        return self.name
+    categories=models.ManyToManyField(Category, verbose_name="Categorias")   
+    class Meta:
+            verbose_name = "Producto"
+            verbose_name_plural = "Productos"
+            ordering = ['name']
+
+            def __str__(self):
+                return self.name
         

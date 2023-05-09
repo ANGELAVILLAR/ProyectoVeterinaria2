3bc57ef328a264761daa30786c6505fbc495914e
BBI\cristian.ascencio <101849129+cristianascenc202505@users.noreply.github.com>
2023-05-09 17:55:38 -0500

    terminado proyecto

--- core/urls.py
+++ core/urls.py
@@ -10,3 +10,4 @@ urlpatterns = [
     path('portfolio/', views.portfolio, name="portfolio"),    
     path('contact/', views.contact, name="contact"),
 ]
+urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- core/views.py
+++ core/views.py
@@ -1,17 +1,21 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
 from carro.carro import Carro
 from .models import Contact
+
 
 def home(request):
     carro=Carro(request)
     return render(request, "core/home.html")
 
+
 def about(request):
     return render(request, "core/about.html")
 
+
 def portfolio(request):
     return render(request, "core/portfolio.html")
+
 
 def contact(request):
     contacts=Contact.objects.all()
